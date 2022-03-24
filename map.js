e922c3c7496fd119c1760537add0d156cfc20e73
Bruce <brucer5f@gmail.com>
2022-03-24 08:40:05 -0700

    mass update

--- map.js
+++ map.js
@@ -1,57 +1,140 @@
+let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
+
+//map
 let map;
 let canvas;
-const mappa = new Mappa('Leaflet');
+var mappa = new Mappa('Leaflet');
 
 const options = {
   lat: 40,
   lng: -95,
   zoom: 4,
-  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",//"https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
 }
+
+//lat long data
+var latsAndLongs = [];
+var canvasCoords = [];
+
+//state variables
+var pinImg;
+var time = 0.0;
+
+
+//Get lat long
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      //alert user that geotagging doesn't work
+    }
+  }
+  
+  //Store lat long
+  function showPosition(position) {
+    var i = latsAndLongs.length;
+    latsAndLongs[i] = new Array();
+    latsAndLongs[i].x = position.coords.latitude;
+    latsAndLongs[i].y = position.coords.longitude;
+
+    generateCoords();
+  }
+
+  //Convert lat long to x,y
+function generateCoords(){
+    
+  for (var i = 0; i < latsAndLongs.length; i++) {
+      canvasCoords[i] = map.latLngToPixel(latsAndLongs[i].x, latsAndLongs[i].y);
+      canvasCoords[i].x -= canvas.width/2;
+      canvasCoords[i].y -= canvas.height/2;
+  }
+}
+
+
+
+//Drawing
+
+function preload() {
+    pinImg = loadImage("pin.png");
+  }
+
 
 function setup(){
     
-    canvas = createCanvas(1000,600, WEBGL);
+  document.getElementById('geolocate').addEventListener(touchEvent, getLocation);
+    
+    var w = document.getElementById("canvasDiv").offsetWidth;
+    var h = document.getElementById("canvasDiv").offsetHeight;
+    canvas = createCanvas(w,h, WEBGL);
+    //canvas.style('position', 'absolute')
+    canvas.parent('canvasDiv');
     frameRate(60);
+
+
+    latsAndLongs[0] = new Array();
+    latsAndLongs[0].x = 32.368407;
+    latsAndLongs[0].y = -111.121668;
     
     map = mappa.tileMap(options); 
     map.overlay(canvas) 
     
-    // Do stuff when map is moved
-    map.onChange(mapMoved);
+    map.onChange(generateCoords);
+
 }
 
-var time = 0.0, radius = 25;
+  //can't resize canvas normally because fuck Mappa
+  //resize flag triggers refresh upon next draw
+var resized = false;
+function windowResized() {
+  console.log("reload");
+  resized = true;
 
-var conti = [], contiCanvasLoc = [];
+
+  /*
+  var w = document.getElementById("canvasDiv").offsetWidth;
+  var h = document.getElementById("canvasDiv").offsetHeight;
+  resizeCanvas(w, h);
+
+  //map.resize(canvas);
+    //map.rm() 
+
+  generateCoords();
+*/
+}
+
+
 
 function draw(){
-    background(0, 0, 0);
-    clear();
+  clear();
+
+  if (resized) {
+    window.location.reload();
+  resized = false;
+}
     
-    radius = 15;
-    
-    noFill();
-    stroke(0);
-    strokeWeight(3);
-    circle(contiCanvasLoc.x, contiCanvasLoc.y, radius+3);
-    circle(contiCanvasLoc.x, contiCanvasLoc.y, 42);
-    
-    fill(0);
-    noStroke();
-    circle(contiCanvasLoc.x, contiCanvasLoc.y, 5);
-    for (var i = 0; i < 1.0; i += 0.1) {
-        circle(contiCanvasLoc.x + radius*cos(i*PI*2 + time), contiCanvasLoc.y + radius*sin(i*PI*2 + time), 8);
+  generateCoords();
+    radius = 5;
+
+    for (var i = 0; i < canvasCoords.length; i++) {
+      drawBeacon(canvasCoords[i].x, canvasCoords[i].y, time);
+      drawPin(canvasCoords[i].x, canvasCoords[i].y);
     }
     
-    time += 0.05;
+    time += 0.03;
 }
 
+function drawBeacon(x, y, t) {
+  var p = t%1;
+  noFill();
+  stroke(0, 100, 155, 255*(1-p));
+  circle(x, y, 35*p);
 
-function mapMoved(){
-    //load list of locations
-    
-    conti = map.latLngToPixel(32.368407, -111.121668);
-    contiCanvasLoc.x = conti.x - canvas.width/2;
-    contiCanvasLoc.y = conti.y - canvas.height/2;
+}
+
+function drawPin(x, y) {
+
+  var w = 25, h = 35;
+  texture(pinImg);
+  noStroke();
+  rect(x - w/2, y - h, w, h);
 }
